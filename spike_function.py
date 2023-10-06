cf0303d1fe2569f2fcbe8824d5ec9c93a665fc51
zero <1684713951@qq.com>
2023-10-06 21:17:52 +0800

    完成监视点扫描、表达式求值

--- spike_function.py
+++ spike_function.py
@@ -22,6 +22,7 @@ class pyspike(pexpect.spawn):
            'a0','a1','a2','a3','a4','a5','a6','a7',
            's0','s1','s2','s3','s4','s5','s6','s7','s8','s9','s10','s11'])
     watch = []
+    watch_last = []
     def reg_get(self):
         self.sendline('reg 0 ')
         self.expect(['\n\('])
@@ -39,7 +40,7 @@ class pyspike(pexpect.spawn):
         self.expect(['\n\('])
         self.reg['pc']=int(self.before[-19:-1],16)
         
-    def read_reg(self,reg_name:str=None):
+    def reg_read(self,reg_name:str=None)-> int:
         """
         读取指定寄存器
         """
@@ -48,21 +49,60 @@ class pyspike(pexpect.spawn):
                 print('寄存器状态未获取')
             else:
                 print(reg_name + ' : 0x{:0>18X}'.format(self.reg[reg_name]))
+                return self.reg[reg_name]
         else:
             print('未找到该寄存器：%s'%reg_name)
-            
+        
     def run(self,steps:int = 1):
+        """
+        运行，默认运行1步
+        """
         for i in range(steps):
             self.sendline('')
             self.expect(['\n\('])
             self.reg_get()
             print(self.reg)
             
+    def watch_append(self,watch_name:str = None):
+        """
+        添加监视点，支持寄存器表达式求值，表达式要求为bool类型
+        """
+        if isinstance(eval(watch_name,{},self.reg),bool):
+            self.watch.append(watch_name)
+            self.watch_last.append(None)
+        else:
+            print('表达式错误，结果需要为bool类型')
             
-w=pyspike('spike -d pk abc',encoding="utf-8")
+    def watch_check(self)->bool:
+        text=''
+        for i in range(len(self.watch)):
+            x = eval(self.watch[i],{},self.reg)
+            if self.watch_last[i] == None:
+                text+=('监视点%d: \'%s\' '%(i,self.watch[i]) + 'None --> %d\n'%x)
+                self.watch_last[i]=x
+                print(self.watch_last[i])
+            elif x == self.watch_last[i]:
+                continue
+            else:
+                text+=('监视点%d: \'%s\' '%(i,self.watch[i]) +'%d --> %d\n'%(self.watch_last[i],x))
+                self.watch_last[i]=x
+        if text=='':
+            return False
+        else:    
+            print('监视点更新!\n'+text)
+            return True
+        
+w = pyspike('spike -d pk abc',encoding="utf-8")
 w.reg_get()
-w.read_reg('a0')
+w.reg_read('pc')
+w.watch_append('pc-2==4098')
+w.watch_append('pc-2==4098')
+w.watch_check()
+w.watch_check()
 w.run()
+w.watch_check()
+
+
 
 
 
